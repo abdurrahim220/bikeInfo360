a49c4ec7c4e8f43a8a839cc8f96f845266e89450
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-11-15 17:59:41 +0600

    some error fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, 
+  swcMinify: true,       
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
+  images: {
+    domains: ["example.com"], 
+  },
 };
 
 export default nextConfig;
