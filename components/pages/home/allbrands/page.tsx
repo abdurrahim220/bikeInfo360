897078ff34e725842b85ebd3d95f240ba17b2c3b
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-11-15 08:08:28 +0600

    brans all adeed

--- components/pages/home/allbrands/page.tsx
+++ components/pages/home/allbrands/page.tsx
@@ -7,8 +7,10 @@ const AllBrands = () => {
   return (
     <div>
       <WrapContainer>
-        <h2 className="text-3xl font-bold text-center my-4">Browse By All Brands</h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <h2 className="text-3xl font-bold text-center my-4">
+          Browse By All Brands
+        </h2>
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-9 gap-4">
           {brandList.map((brand) => (
             <BrandCard key={brand.id} title={brand.title} image={brand.image} />
           ))}
--- lib/brands.ts
+++ lib/brands.ts
@@ -16,4 +16,34 @@ export const brandList: BrandType[] = [
     image: "/brands/suzuki.png",
     title: "Suzuki",
   },
+  {
+    id: 4,
+    image: "/brands/royal-enfield.png",
+    title: "Royal Enfield",
+  },
+  {
+    id: 5,
+    image: "/brands/ktm-2.png",
+    title: "Ktm",
+  },
+  {
+    id: 6,
+    image: "/brands/yamaha.png",
+    title: "Yamaha",
+  },
+  {
+    id: 7,
+    image: "/brands/bajaj.png",
+    title: "Bajaj",
+  },
+  {
+    id: 8,
+    image: "/brands/hero.png",
+    title: "Hero",
+  },
+  {
+    id: 9,
+    image: "/brands/lifan.png",
+    title: "Lifan",
+  },
 ];
